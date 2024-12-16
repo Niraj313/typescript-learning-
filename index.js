b19f0b7a9c25197a364353ff0230769a969d94c0
Niraj Gupta <coder4117@gmail.com>
2024-12-16 20:15:03 +0530

    Types in Typescript

--- index.js
+++ index.js
@@ -6,4 +6,18 @@ function hello(name) {
     console.log(`hello ${name}`);
 }
 hello('Niraj');
+function sum(a, b) {
+    return a + b;
+}
+const value = sum(1, 2);
+console.log(value);
+function isAge(age) {
+    if (age > 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isAge(18));
 // --------------
--- index.ts
+++ index.ts
@@ -6,4 +6,19 @@ function hello(name:String) {
     console.log(`hello ${name}`);
 }
 hello('Niraj');
+
+function sum(a:number, b:number):number  {
+    return a + b;
+}
+const value = sum(1, 2);
+console.log(value);
+
+function isAge(age:number):boolean {
+    if (age > 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isAge(18));
 // --------------
